946d4ee415ef0b54a5e88afad104e56822950326
leanhhuy0709 <78292068+leanhhuy0709@users.noreply.github.com>
2023-04-02 12:11:34 +0700

    Delete Model directory

--- Model/watch.sql
+++
@@ -1,7 +0,0 @@
-USE `oliviashop`;
-
-SELECT * FROM CUSTOMER;
-SELECT * FROM admin;
-select * from supplier;
-select * from category;
-select * from product left join image on product.productID = image.productID;
